aa66e76415e9e7f7d44581cbd3620fd7ed150775
dingdingbaba <122307348@qq.com>
2019-06-04 22:33:24 +0800

    for design pattern

---
+++ WinForm/DesignPattern/DesignPattern/Observer/ConcreteSubject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Observer
+{
+    public class ConcreteSubject : Subject
+    {
+        private string _subjectState;
+
+        /// <summary>
+        /// Gets or sets subject state
+        /// </summary>
+        public string SubjectState
+        {
+            get { return _subjectState; }
+            set { _subjectState = value; }
+        }
+    }
+}
---
+++ WinForm/DesignPattern/DesignPattern/Observer/FormObserver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesignPattern.Observer
+{
+    public partial class FormObserver : Form
+    {
+        public FormObserver()
+        {
+            InitializeComponent();
+        }
+
+        private void FormObserver_Load(object sender, EventArgs e)
+        {
+            // Configure Observer pattern
+            ConcreteSubject s = new ConcreteSubject();
+
+            s.Attach(new ConcreteObserver(s, "X"));
+            s.Attach(new ConcreteObserver(s, "Y"));
+            s.Attach(new ConcreteObserver(s, "Z"));
+
+            // Change subject and notify observers
+            s.SubjectState = "ABC";
+            s.Notify();
+        }
+    }
+}
